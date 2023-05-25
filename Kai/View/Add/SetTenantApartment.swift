f548584c573ba031441c6a668957ec5d1e427d3c
scolas <scolas481@gmail.com>
2023-05-25 00:01:31 -0400

    property view

---
+++ Kai/View/Add/SetTenantApartment.swift
@@ -0,0 +1,20 @@
+//
+//  SetTenantApartment.swift
+//  Kai
+//
+//  Created by Scott Colas on 12/25/22.
+//
+
+import SwiftUI
+
+struct SetTenantApartment: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SetTenantApartment_Previews: PreviewProvider {
+    static var previews: some View {
+        SetTenantApartment()
+    }
+}
